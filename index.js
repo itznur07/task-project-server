3eb819be2e2dbf69cbe5c9e003dbbca2324b448e
Md Nuruddin <nuruddinalways99@gmail.com>
2023-08-05 11:49:46 +0600

    Post and Get Api created

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+/** Middlewares */
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@todos.ukwfq5e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const usersDataCollection = client.db("hkdb").collection("userdata");
+
+    app.get("/userdata", async (req, res) => {
+      const result = await usersDataCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/userdata", async (req, res) => {
+      const data = req.body;
+      const existData = await usersDataCollection.findOne(data);
+      if (!existData) {
+        const result = await usersDataCollection.insertOne(data);
+        res.send(result);
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
